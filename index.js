5e718e7f0ec3b84bfefd813d12b7ecef99a49cdc
Nikita Kolotilo <nikita.kolotilo@gmail.com>
2019-04-23 12:08:32 +0300

    Changing PORT once again

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const controller = Botkit.facebookbot({
 
 const bot = controller.spawn({});
 
-controller.setupWebserver(process.env.port, (err, webserver) => {
+controller.setupWebserver(process.env.PORT || 5000, (err, webserver) => {
   controller.createWebhookEndpoints(webserver, bot, () => {
     console.log('ONLINE!');
   });
